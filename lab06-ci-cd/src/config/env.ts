449355aacdf0251f7a44ccd109b36e5a81662fec
khodakivskiy <andriyhodak2007@gmail.com>
2026-04-25 12:31:28 +0300

    lab06: update environment configuration to load dotenv only in non-production mode

--- lab06-ci-cd/src/config/env.ts
+++ lab06-ci-cd/src/config/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import {envSchema} from "../schemas/env.schema";
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
 
